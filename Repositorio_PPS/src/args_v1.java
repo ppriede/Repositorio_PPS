630d21be12079483caf5350ab07df7da34a3147a
pprie <pprie@PPS-SOOS>
2017-07-23 17:10:27 -0400

    Primer intento de commit archivo

---
+++ Repositorio_PPS/src/args_v1.java
@@ -0,0 +1,12 @@
+//Muestra el primer argumento ingresado por el usuario al ejecutar.
+import java.io.*;
+public class args_v1 
+{ 
+	public static void main(String... args) throws FileNotFoundException, IOException, NullPointerException {
+		System.out.println("Args[0]: "+args[0].toString());
+		integrar(args[0]);
+	}
+	public static void integrar(String integrar) throws FileNotFoundException, IOException, NullPointerException {
+		System.out.println("Dentro de funcion, integrar: "+ integrar);
+	}
+}
