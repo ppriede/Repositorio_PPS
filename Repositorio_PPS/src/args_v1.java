75067c3d68f9e10d67d500770dd2b3494a011cc6
ppriede <ppriede@PPS-SOOS>
2017-07-23 17:19:30 -0400

    Agregamos 2 args mas, solo para jugar.

--- Repositorio_PPS/src/args_v1.java
+++ Repositorio_PPS/src/args_v1.java
@@ -3,11 +3,13 @@ import java.io.*;
 public class args_v1 
 { 
 	public static void main(String... args) throws FileNotFoundException, IOException, NullPointerException {
-		System.out.println("Args[0]: "+args[0].toString());
+		System.out.println("args[0]: "+args[0].toString());
+		System.out.println("args[1]: "+args[1].toString());
+		System.out.println("args[2]: "+args[2].toString());
 		integrar(args[0]);
 	}
 	public static void integrar(String integrar) throws FileNotFoundException, IOException, NullPointerException {
-		System.out.println("Dentro de funcion, integrar: "+ integrar);
+		System.out.println("Dentro de funcion, integrar (solo primer arg): "+ integrar);
 	}
 }
 
